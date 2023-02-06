e73aa1cd30f7b2c5b8c9456ecb20d76de40b993e
Splatura Designs <30926671+splatura@users.noreply.github.com>
2023-02-06 14:39:58 +1100

    Create configure_proxmox.sh

---
+++ configure_proxmox.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+sed -i 's#^\(GRUB_CMDLINE_LINUX_DEFAULT="quiet\)"$#\1 amd_iommu=on iommu=pt"#' /etc/default/grub
+update_grub
+
+echo "options kvm ignore_msrs=1 report_ignored_msrs=0" > /etc/modprobe.d/kvm.conf
+
+echo "options vfio-pci ids=10de:2507,10de:228e disable_vga=1" > /etc/modprobe.d/vfio.conf
+
+echo "blacklist nvidiafb" >> /etc/modprobe.d/pve-blacklist.conf
+echo "blacklist nouveau" >> /etc/modprobe.d/pve-blacklist.conf
+echo "blacklist nvidia" >> /etc/modprobe.d/pve-blacklist.conf
+echo "blacklist snd_hda_codec_hdmi" >> /etc/modprobe.d/pve-blacklist.conf
+echo "blacklist snd_hda_intel" >> /etc/modprobe.d/pve-blacklist.conf
+echo "blacklist snd_hda_codec" >> /etc/modprobe.d/pve-blacklist.conf
+echo "blacklist snd_hda_core" >> /etc/modprobe.d/pve-blacklist.conf
+echo "blacklist radeon" >> /etc/modprobe.d/pve-blacklist.conf
+echo "blacklist amdgpu" >> /etc/modprobe.d/pve-blacklist.conf
+
+update-initramfs -u -k all
+md /var/lib/vz/snippets
