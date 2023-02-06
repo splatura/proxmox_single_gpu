87f29f774fba01954897b963a4f2c304fad0a5b1
Splatura Designs <30926671+splatura@users.noreply.github.com>
2023-02-06 15:30:42 +1100

    Update configure_proxmox.sh

--- configure_proxmox.sh
+++ configure_proxmox.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-echo "# PVE pve-no-subscription repository provided by proxmox.com," >> /etc/apt/sources.list
-echo "# NOT recommended for production use" >> /etc/apt/sources.list
-echo "deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription" >> /etc/apt/sources.list
-
-apt update
-apt dist-upgrade -y
-
 sed -i 's#^\(GRUB_CMDLINE_LINUX_DEFAULT="quiet\)"$#\1 amd_iommu=on iommu=pt"#' /etc/default/grub
 update_grub
 
